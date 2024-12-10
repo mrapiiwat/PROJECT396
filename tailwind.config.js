186c3a9fd7340381c8d81d08b83023927ec7629e
Apiwat <mrapiiwat@gmail.com>
2024-12-10 10:27:01 +0700

    feat: remove Tailwind CSS configuration file

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // รวมไฟล์ JSX ทั้งหมด
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
