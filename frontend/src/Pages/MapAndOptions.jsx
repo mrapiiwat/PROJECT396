7850515d6eee34df32dda08b8a7bdb322f2ef8cb
Naruemon55 <narimon1101@gmail.com>
2024-12-13 16:47:04 +0700

    ..

--- frontend/src/Pages/MapAndOptions.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MapAndOptions = () => {
-  return (
-    <div>MapAndOptions</div>
-  )
-}
-
-export default MapAndOptions
--- frontend/src/Pages/RideOptions.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RideOptions = () => {
-  return (
-    <div>RideOptions</div>
-  )
-}
-
-export default RideOptions
